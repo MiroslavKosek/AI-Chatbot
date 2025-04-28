bd6703ebb222aee8b5e2d0757b6a81ce8d2bff72
Miroslav Košek <91048122+MiroslavKosek@users.noreply.github.com>
2025-04-28 20:43:51 +0200

    Update index.tsx (#18)

--- client/components/web/footer/index.tsx
+++ client/components/web/footer/index.tsx
@@ -2,7 +2,7 @@ export default function Footer(){
     return (
         <div className="fixed bottom-0 left-0 w-full bg-black text-white p-5">
             <div className="font-poppins font-normal">
-                Created by students of the Technical University of Liberec as part of Ideathon.
+                Created by students of the Technical University of Liberec as part of Ideathon 2025.
             </div>
         </div>
     );
